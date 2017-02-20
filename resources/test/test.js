fcfdd111a04ebe0e7e8cd52d3f47ddce989a8481
Andrew Cheng <acheng@localhost.localdomain>
2017-02-20 11:17:15 -0800

    fix travis?

--- resources/test/test.js
+++ resources/test/test.js
@@ -4,12 +4,13 @@ var page = require('webpage').create();
 // phantom.args[0] is deprecated as of phantomJS 2.0
 // http://phantomjs.org/api/phantom/property/args.html
 var system = require('system');
-var argsdeprecated = system.args;
-argsdeprecated.shift();
-phantom.args = argsdeprecated;
+// var argsdeprecated = system.args;
+// argsdeprecated.shift();// travis says: RangeError: Range error
+// phantom.args = argsdeprecated;
 ////////////////////////////////////////////////////////////////////////////////
 
-var url = phantom.args[0];
+//var url = phantom.args[0];
+var url = system.args[1];
 
 page.onConsoleMessage = function (message) {
     console.log(message);
